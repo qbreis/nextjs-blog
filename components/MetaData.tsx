26eaa8c674d1d8e93b8702fc68ffbdb1cfbf63a5
qbreis <qbreis@gmail.com>
2022-06-29 15:55:26 +0200

    add stylesheet by relative path to root

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -2,28 +2,37 @@ import Head from 'next/head';
 
 import nextConfig from '../next.config';
 
+import { useRouter } from 'next/router'
+
 const MetaData = ({title, description,}: any) => {
+    const router = useRouter();
+    const routerAsPath = router.asPath;
+    const pathToRootArray = routerAsPath.split('/');
+    let pathToRoot = '';
+    for(let i = 1; i < pathToRootArray.length; i++){
+        if(routerAsPath != '/'){ pathToRoot += '../'; }
+    }
+    const staticStylesCss = pathToRoot+'static-styles.css';
+
     return (
-      <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <meta name='description' content={description} />
-        <meta charSet='utf-8' />
-        <link rel='icon' href='/favicon.png' />
-        <title>{title}</title>
-        {/*
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap"
-          rel="stylesheet"
-        />
-        <link href="static-styles.css" rel="stylesheet" />
-        */}
-      </Head>
+        <Head>
+            <meta name='viewport' content='width=device-width, initial-scale=1' />
+            <meta name='description' content={description} />
+            <meta charSet='utf-8' />
+            <link rel='icon' href='/favicon.png' />
+            <title>{title}</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link
+                rel="preconnect"
+                href="https://fonts.gstatic.com"
+                crossOrigin="true"
+            />
+            <link
+                  href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap"
+                  rel="stylesheet"
+            />
+            <link href={staticStylesCss} rel="stylesheet" />
+        </Head>
     );
 };
 
