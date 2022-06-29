4f173c5c9d5ade3327bc15329fcb3c279162cec1
qbreis <qbreis@gmail.com>
2022-06-29 16:14:09 +0200

    add _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,32 @@
 // pages/_document.js
 
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { useRouter } from 'next/router'
 
 class MyDocument extends Document {
   render() {
+    const router = useRouter();
+    const routerAsPath = router.asPath;
+    const pathToRootArray = routerAsPath.split('/');
+    let pathToRoot = '';
+    for(let i = 1; i < pathToRootArray.length; i++){
+        if(routerAsPath != '/'){ pathToRoot += '../'; }
+    }
+    const staticStylesCss = pathToRoot+'static-styles.css';
     return (
       <Html>
         <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=optional"
-            rel="stylesheet"
-          />
-
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link
+                rel="preconnect"
+                href="https://fonts.gstatic.com"
+                crossOrigin="true"
+            />
+            <link
+                  href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap"
+                  rel="stylesheet"
+            />
+            <link href={staticStylesCss} rel="stylesheet" />
 
         </Head>
         <body>
