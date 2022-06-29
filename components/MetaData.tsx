e5bda6963c070765fe0c2cdb9a42d10e58609340
qbreis <qbreis@gmail.com>
2022-06-29 16:19:31 +0200

    add _document.tsx

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -2,17 +2,7 @@ import Head from 'next/head';
 
 import nextConfig from '../next.config';
 
-import { useRouter } from 'next/router'
-
 const MetaData = ({title, description,}: any) => {
-    const router = useRouter();
-    const routerAsPath = router.asPath;
-    const pathToRootArray = routerAsPath.split('/');
-    let pathToRoot = '';
-    for(let i = 1; i < pathToRootArray.length; i++){
-        if(routerAsPath != '/'){ pathToRoot += '../'; }
-    }
-    const staticStylesCss = pathToRoot+'static-styles.css';
 
     return (
         <Head>
@@ -21,17 +11,7 @@ const MetaData = ({title, description,}: any) => {
             <meta charSet='utf-8' />
             <link rel='icon' href='/favicon.png' />
             <title>{title}</title>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link
-                rel="preconnect"
-                href="https://fonts.gstatic.com"
-                crossOrigin="true"
-            />
-            <link
-                  href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap"
-                  rel="stylesheet"
-            />
-            <link href={staticStylesCss} rel="stylesheet" />
+            
         </Head>
     );
 };
---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+import { useRouter } from 'next/router';
+
+export default function Document() {
+    const router = useRouter();
+    const routerAsPath = router.asPath;
+    const pathToRootArray = routerAsPath.split('/');
+    let pathToRoot = '';
+    for(let i = 1; i < pathToRootArray.length; i++){
+        if(routerAsPath != '/'){ pathToRoot += '../'; }
+    }
+    const staticStylesCss = pathToRoot+'static-styles.css';
+    return (
+        <Html>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossOrigin="true"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap"
+                    rel="stylesheet"
+                />
+                <link href={staticStylesCss} rel="stylesheet" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
