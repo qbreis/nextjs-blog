38cb787e4844af7a3a1b7260bd6b94bb499392ba
qbreis <qbreis@gmail.com>
2022-06-29 16:36:11 +0200

    add deploypath to config js

--- next.config.js
+++ next.config.js
@@ -3,10 +3,22 @@
 const siteTitle = 'qbreis — enric gatell';
 const siteDescription = 'This blog contains the step-by-step annotations of what I learn and consolidate, day by day, in terms of programming and web design, among other things.';
 
+let deployPath = 'http://localhost:3000';
+if (process.env.NETLIFY === 'true') {
+    deployPath = process.env.URL;
+    if (
+        process.env.CONTEXT === 'deploy-preview' ||
+        process.env.CONTEXT === 'staging'
+    ) {
+        deployPath = process.env.DEPLOY_PRIME_URL;
+    }
+}
+
 const nextConfig = {
-  reactStrictMode: true,
-  siteTitle: siteTitle,
-  siteDescription: siteDescription,
+    reactStrictMode: true,
+    deployPath: deployPath,
+    siteTitle: siteTitle,
+    siteDescription: siteDescription,
 }
 
 module.exports = nextConfig
