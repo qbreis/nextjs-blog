15ff751ec631fe3550240ceae7e3f6a9830b5733
Enric <qbreis@gmail.com>
2022-06-29 17:53:08 +0000

    fix: load custom typo in Head component intead of Document

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -2,8 +2,18 @@ import Head from 'next/head';
 
 import nextConfig from '../next.config';
 
-const MetaData = ({title, description,}: any) => {
+import { useRouter } from 'next/router';
 
+const MetaData = ({title, description,}: any) => {
+    const router = useRouter();
+    const routerAsPath = router.asPath;
+    const pathToRootArray = routerAsPath.split('/');
+    let pathToRoot = '';
+    for(let i = 1; i < pathToRootArray.length; i++){
+        if(routerAsPath != '/'){ pathToRoot += '../'; }
+    }
+    const staticStylesCss = pathToRoot+'static-styles.css';
+    console.log('staticStylesCss: '+staticStylesCss);
     return (
         <Head>
             <meta name='viewport' content='width=device-width, initial-scale=1' />
@@ -11,7 +21,7 @@ const MetaData = ({title, description,}: any) => {
             <meta charSet='utf-8' />
             <link rel='icon' href='/favicon.png' />
             <title>{title}</title>
-            
+            <link href={staticStylesCss} rel="stylesheet" />
         </Head>
     );
 };
